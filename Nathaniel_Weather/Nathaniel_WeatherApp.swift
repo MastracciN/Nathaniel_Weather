57e0b78fb3572a74b7b645a0fe1c5ff2bb57b453
bob <bob@bob.bob>
2021-11-13 11:08:13 -0500

    Initial Commit

---
+++ Nathaniel_Weather/Nathaniel_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  Nathaniel_WeatherApp.swift
+//  Nathaniel_Weather
+//
+//  Created by user929441 on 11/13/21.
+//
+
+import SwiftUI
+
+@main
+struct Nathaniel_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
