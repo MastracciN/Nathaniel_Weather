43f8616d4f87d621fe453eb9de56d1fada950a13
bob <bob@bob.bob>
2021-11-13 15:44:41 -0500

    weather model functionality implemented

--- Nathaniel_Weather/Models/Weather.swift
+++ Nathaniel_Weather/Models/Weather.swift
@@ -10,5 +10,77 @@ import Foundation
 struct Weather: Codable, Identifiable{
     var temp : Double
     var feelsLike : Double
+    var windKph : Double
+    var windDir : String
+    var humidity : Int
+    var uv : Double
+    var vis : Double
+    var conditionText : String
     
+    var name: String
+    var country : String
+    
+    enum CodingKeys: String, CodingKey{
+        case temp = "temp_c"
+        case feelsLike = "feelslike_c"
+        case windKph = "wind_kph"
+        case windDir = "wind_dir"
+        case humidity = "humidity"
+        case uv = "uv"
+        case vis = "vis_km"
+        case conditionText = "text"
+        case condition = "condition"
+        case current = "current"
+        
+        case name = "name"
+        case country = "country"
+        case location = "location"
+    }
+    
+    init (){
+        temp = 0.0
+        feelsLike = 0.0
+        windKph = 0.0
+        windDir = "whoosh"
+        humidity = 0
+        uv = 0.0
+        vis = 0.0
+        conditionText = "upupup"
+    }
+    
+    init(from decoder: Decoder) throws {
+        let response = try decoder.container(keyedBy: CodingKeys.self)
+        
+        self.temp = try response.decodeIfPresent(Double.self, forKey: .temp) ?? 0
+        self.feelsLike = try response.decodeIfPresent(Double.self, forKey: .feelsLike) ?? 0
+        self.windKph = try response.decodeIfPresent(Double.self, forKey: .windKph) ?? 0
+        self.windDir = try response.decodeIfPresent(String.self, forKey: .windDir) ?? "NA"
+        self.humidity = try response.decodeIfPresent(Int.self, forKey: .humidity) ?? 0
+        self.uv = try response.decodeIfPresent(Double.self, forKey: .uv) ?? 0
+        self.vis = try response.decodeIfPresent(Double.self, forKey: .vis) ?? 0
+        
+        self.conditionText = try response.decodeIfPresent(String.self, forKey: .conditionText) ?? "NA"
+        self.name = try response.decodeIfPresent(String.self, forKey: .name) ?? "NA"
+        self.country = try response.decodeIfPresent(String.self, forKey: .country) ?? "NA"
+    }
+    
+    struct Location : Codable {
+        let name : String
+        let country : String
+        
+        enum CodingKeys: String, CodingKey {
+            case name = "name"
+            case country = "country"
+        }
+        
+        init(from decoder: Decoder) throws {
+            let response = try decoder.container(keyedBy: CodingKeys.self)
+            self.name = try response.decodeIfPresent(String.self, forKey: .name) ?? "NA"
+            self.country = try response.decodeIfPresent(String.self, forKey: .country) ?? "NA"
+        }
+    }
+    
+//    struct Current : Codable {
+//        let co
+//    }
 }
--- Nathaniel_Weather/Nathaniel_WeatherApp.swift
+++ Nathaniel_Weather/Nathaniel_WeatherApp.swift
@@ -10,10 +10,13 @@ import SwiftUI
 @main
 struct Nathaniel_WeatherApp: App {
     let locationHelper = LocationHelper()
+    var fetcher = WeatherFetcher()
     
     var body: some Scene {
         WindowGroup {
-            ContentView().environmentObject(locationHelper)
+            ContentView()
+                .environmentObject(locationHelper)
+                .environmentObject(fetcher)
         }
     }
 }
